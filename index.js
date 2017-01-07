9e1c135dda22803eb664446cd54f27ad49211b7c
Mike <mahathaway93@gmail.com>
2017-01-07 16:53:42 -0500

    updated directory structure

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (request, response) => {
   response.render('home', {
